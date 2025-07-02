86761af654631199c20d968d5ab2c4d9047e7858
Harini-devx09 <harinin459@gmail.com>
2025-07-02 13:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import uuid
 import random
 
 app = Flask(__name__)
-app.secret_key = '58413941156d95fe3d08320e7fc75452a351eca2bb1f7eaf841cc6f1ccc2e877' # IMPORTANT: Change this to a strong, random key in production!
+app.secret_key = '0995c847a14c8942d792365678f1dc1dc8cfedb435f6780c69d8c0c8a96dea84' # IMPORTANT: Change this to a strong, random key in production!
 
 # AWS Setup using IAM Role
 REGION = 'us-east-1'  # Replace with your actual AWS region
